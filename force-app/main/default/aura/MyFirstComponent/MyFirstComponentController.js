ee2f8b64f7861bde385abde310b2419349061588
Aishwaryajange <52314674+Aishwaryajange@users.noreply.github.com>
2019-12-09 15:20:57 +0530

    auracomp2

--- force-app/main/default/aura/MyFirstComponent/MyFirstComponentController.js
+++ force-app/main/default/aura/MyFirstComponent/MyFirstComponentController.js
@@ -1,5 +1,13 @@
 ({
-    myAction : function(component, event, helper) {
-
+	doCLickMap: function(component,event,helper){
+        var map = [];
+        //key and value
+        for(var i=0; i<10; i++){
+            map.push({
+                key:i,
+                value:'Test '+i
+            }); 
+        }
+        component.set('v.mapVar',map);
     }
 })
